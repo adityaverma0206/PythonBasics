8561cdde76e8655a529cac577d9f812ea0336409
adityaverma826 <adityaverma0206@gmail.com>
2026-02-01 15:54:33 +0530

    p2

--- basics.py
+++ basics.py
@@ -1,4 +1,17 @@
-pie = 3.147
-radius = float(input("enter radius= "))
-area = pie*radius*radius
-print(f'area of cicle:{area}')
+n1 = float(input("enter no 1="))
+n2 = float (input("enter no 2="))
+choice = input("enter your choice = + / // * ** - ")
+if choice == "+":
+    print(f"add:{n1+n2}")
+elif choice == "-":
+    print(f'sub:{n1-n2}')
+elif choice == "*":
+    print(f'mul:{n1*n2}')
+elif choice == "/":
+    print(f'div:{n1/n2}')
+elif choice == "//":
+    print(f"floatdiv:{n1//n2}")
+elif choice == "**":
+    print(f"exp:{n1**n2}")
+else :
+    print("invalid") 
